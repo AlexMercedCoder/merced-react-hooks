d37da56b318a67aa0989bebe56bdbbbda76e9b6e
Alex Merced <willcodeforfood@alexmerced.com>
2020-10-28 18:41:26 -0400

    update

--- README.md
+++ README.md
@@ -15,14 +15,82 @@ npm install --save merced-react-hooks
 ```jsx
 import React, { Component } from 'react'
 
-import MyComponent from 'merced-react-hooks'
-import 'merced-react-hooks/dist/index.css'
+import {
+  createDataStore,
+  useForm,
+  useLocalStorage,
+  useSessionStorage
+} from 'merced-react-hooks'
+```
 
-class Example extends Component {
-  render() {
-    return <MyComponent />
-  }
+## useDataStore
+
+Pass it the initial state and reducer and it returns a provider component and hook to pull the DataStore state in any component being provided by the provider component.
+
+Create a DS.js, define your initial state and reducer function and
+
+```js
+// Define and initialState and Reducer
+export const [DataStore, useDataStore] = createDataStore(initialState, reducer)
+```
+
+### Wrap Your App Components with DataStore
+
+```tsx
+// App.jsx
+import React, { Component } from 'react'
+import {DataStore} from "./DS.js
+
+const App = (props) => {
+  return <DataStore><OtherComponent/></DataStore>
 }
+```
+
+### Pull data using hook in any component
+
+```tsx
+// /components/component.jsx
+import React from "react'
+import {useDataStore} from "../DS.js"
+
+const Component = (props) => {
+  const {dataStore, dispatch} = useDataStore()
+
+  return <><h1>{dataStore.title}</h1>
+  <button onClick={() => dispatch({type:"something", payload: 5})}>Click Me</button>
+}
+```
+
+## useForm
+
+Pass it the initial form state and it will return an array with the formState, handleChange function and resetForm function. 
+
+```js
+const [formData, handleChange, resetForm] = useForm({
+  name: '',
+  age: 0,
+  email: ''
+})
+
+return <form onSubmit = {(event) => {
+  event.preventDefault()
+  resetForm()
+}}>
+  <input type="text" name="name" onChange={handleChange}>
+  <input type="number" name="age" onChange={handleChange}>
+  <input type="text" name="email" onChange={handleChange}>
+  <input type="submit" value="submit">
+</form>
+```
+
+## useLocalStorage & useSessionStorage
+
+Pass it a key and initialState. It will check session or local storage to see if the key already exists, if it does will create a state with the value of the existing info stored, if it does not exist it will create new state with the initial value.
+
+The function returns the state and a special setState function that updates the local or session storage with each change. Returns an array with state, setState, and a reset function to resetting the state and localStorage to initial.
+
+```js
+const [token, setToken, resetToken] = useLocalStorage('token', null)
 ```
 
 ## License
--- src/index.js
+++ src/index.js
@@ -1,6 +1,109 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+//////////////////////////
+// +& useDataStore
+/////////////////////////
+
+export const createDataStore = (initialState, reducer) => {
+  const DS = React.createContext({})
+
+  const useDataStore = () => {
+    return React.useContext(DS)
+  }
+
+  const DataStore = (props) => {
+    const [dataStore, dispatch] = React.useReducer(reducer, initialState)
+
+    return (
+      <DS.Provider value={{ dataStore, dispatch }}>
+        {props.children}
+      </DS.Provider>
+    )
+  }
+
+  return [DataStore, useDataStore]
+}
+
+//////////////////////////
+// +& useFormState
+/////////////////////////
+
+export const useFormState = (initialState) => {
+  const [formData, setFormData] = React.useState(initialState)
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value })
+  }
+
+  const reset = () => setFormData(initialState)
+
+  return [formData, handleChange, reset]
+}
+
+//////////////////////////
+// +& useLocalStorage
+/////////////////////////
+
+export const useLocalStorage = (key, initial) => {
+  try {
+    const exists = JSON.parse(window.localStorage.getItem(key))
+    let start
+
+    if (exists) {
+      start = exists
+    } else {
+      start = initial
+    }
+
+    const [state, setState] = React.useState(start)
+
+    const setStateStorage = (newState) => {
+      window.localStorage.setItem(key, JSON.stringify(newState))
+      setState(newState)
+    }
+
+    const reset = () => {
+      window.localStorage.removeItem(key)
+      setState(null)
+    }
+    return [state, setStateStorage, reset]
+  } catch (error) {
+    console.log(error)
+    alert(error)
+  }
+}
+
+//////////////////////////
+// +& useSessionStorage
+/////////////////////////
+
+export const useSessionStorage = (key, initial) => {
+  try {
+    const exists = JSON.parse(window.sessionStorage.getItem(key))
+    let start
+
+    if (exists) {
+      start = exists
+    } else {
+      start = initial
+    }
+
+    const [state, setState] = React.useState(start)
+
+    const setStateStorage = (newState) => {
+      window.sessionStorage.setItem(key, JSON.stringify(newState))
+      setState(newState)
+    }
+
+    const reset = () => {
+      window.sessionStorage.removeItem(key)
+      setState(null)
+    }
+
+    return [state, setStateStorage, reset]
+  } catch (error) {
+    console.log(error)
+    alert(error)
+  }
 }
