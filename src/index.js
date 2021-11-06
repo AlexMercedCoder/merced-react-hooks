0f5c1782462e6ba306746e5c967d00647312e5a6
Alex Merced <alex@alexmerced.dev>
2021-11-06 13:46:28 -0400

    condition fix

--- src/index.js
+++ src/index.js
@@ -432,12 +432,19 @@ export const useCrud = (url, headers = {}) => {
 export const Condition = (props) => {
   let wrong = props.ifnot ?? null
   wrong = wrong instanceof Function ? wrong() : wrong
+  let result
+  console.log('Children', props.children)
+  console.log('Wrong', wrong)
 
   if (props.children instanceof Function) {
-    return props.if ? props.children() : wrong
+    result = props.if ? props.children() : wrong
   } else {
-    return props.if ? props.children : wrong
+    result = props.if ? props.children : wrong
   }
+
+  console.log(result)
+
+  return result ? result : <h1>Something Wrong</h1>
 }
 
 // /////////////////////////////////
