8698a9adb6f41f286557934be7b28b25d774e496
Alex Merced <alex@alexmerced.dev>
2021-11-06 13:51:40 -0400

    condition fix

--- src/index.js
+++ src/index.js
@@ -433,16 +433,12 @@ export const Condition = (props) => {
   let wrong = props.ifnot ?? null
   wrong = wrong instanceof Function ? wrong() : wrong
   let result
-  console.log('Children', props.children)
-  console.log('Wrong', wrong)
 
   if (props.children instanceof Function) {
     result = props.if ? props.children() : wrong
   } else {
     result = props.if ? props.children : wrong
   }
-
-  console.log(result)
 
   return result ? result : <h1>Something Wrong</h1>
 }
